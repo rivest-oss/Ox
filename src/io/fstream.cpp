fd680b233c19e96a3220e42aac9e33aaf9f331be
Rivest Osz <rivest-oss@proton.me>
2025-04-15 13:27:10 -0300

    Adding File Stream

---
+++ src/io/fstream.cpp
@@ -0,0 +1,185 @@
+/* Ox: a general-purpose library.
+** Copyright (C) 2024-2025  Rivest Osz
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "fstream.hpp"
+#include <fstream>
+#include <cerrno>
+#include <cstring>
+
+namespace Ox {
+	std::ios::openmode __ox_impl_filestream_openmode(openmode mode) {
+		std::ios::openmode smode = std::ios::binary;
+
+		if(mode & Ox::openmode::in) smode |= std::ios::in;
+		if(mode & Ox::openmode::out) smode |= std::ios::out;
+
+		return smode;
+	};
+
+	std::ios::seekdir __ox_impl_filestream_seekdir(seekdir dir) {
+		if(dir == Ox::seekdir::cur) return std::ios::cur;
+		if(dir == Ox::seekdir::end) return std::ios::end;
+
+		return std::ios::beg;
+	};
+
+	int FileStream::open(const char *path, openmode mode, const char **err) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(*err != nullptr)
+			return -1;
+
+		if(f != nullptr && is_open()) {
+			*err = "File stream is already open";
+			return -1;
+		}
+
+		if(path == nullptr) {
+			*err = "Null 'path'";
+			return -1;
+		}
+
+		if(f == nullptr) {
+			f = inhale<std::fstream>(err);
+			if(*err != nullptr)
+				return -1;
+
+			new (f) std::fstream();
+		}
+
+		__ox_implptr = f;
+
+		f->clear();
+		f->close();
+		f->open(path, __ox_impl_filestream_openmode(mode));
+		if(f->fail()) {
+			*err = std::strerror(errno);
+			close();
+			return -1;
+		}
+
+		return 0;
+	};
+
+	bool FileStream::is_open(void) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return false;
+		
+		return f->is_open();
+	};
+
+	void FileStream::close(void) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return;
+
+		if(is_open())
+			f->close();
+
+		f->~basic_fstream();
+		exhale(f);
+		__ox_implptr = nullptr;
+	};
+
+	ulong FileStream::tellg(void) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return -1;
+		
+		return static_cast<ulong>(f->tellg());
+	};
+
+	void FileStream::seekg(ulong pos) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return;
+
+		f->seekg(pos);
+	};
+
+	void FileStream::seekg(long off, seekdir dir) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return;
+
+		f->seekg(off, __ox_impl_filestream_seekdir(dir));
+	};
+
+	ulong FileStream::tellp(void) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return -1;
+		
+		return static_cast<ulong>(f->tellp());
+	};
+
+	void FileStream::seekp(ulong pos) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return;
+
+		f->seekp(pos);
+	};
+
+	void FileStream::seekp(long off, seekdir dir) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr)
+			return;
+
+		f->seekp(off, __ox_impl_filestream_seekdir(dir));
+	};
+
+	int FileStream::read(u8 *s, ulong n, const char **err) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr) {
+			*err = "Unitialized FileStream implementation";
+			return -1;
+		}
+
+		if(*err != nullptr)
+			return -1;
+
+		(void)f->read((char *)s, n);
+
+		if(f->fail() && errno != 0) {
+			*err = std::strerror(errno);
+			return -1;
+		}
+
+		return 0;
+	};
+
+	int FileStream::write(u8 *s, ulong n, const char **err) {
+		std::fstream *f = (std::fstream *)__ox_implptr;
+		if(f == nullptr) {
+			*err = "Unitialized FileStream implementation";
+			return -1;
+		}
+
+		if(*err != nullptr)
+			return -1;
+
+		(void)f->write((char *)s, n);
+
+		if(f->fail() && errno != 0) {
+			*err = std::strerror(errno);
+			return -1;
+		}
+
+		return 0;
+	};
+};
---
+++ src/io/fstream.hpp
@@ -0,0 +1,46 @@
+/* Ox: a general-purpose library.
+** Copyright (C) 2024-2025  Rivest Osz
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#pragma once
+#include "stream.hpp"
+
+namespace Ox {
+	class FileStream : public BasicIOStream {
+		private:
+			void *__ox_implptr = nullptr;
+		
+		public:
+			~FileStream(void) {
+				close();
+			};
+
+			int open(const char *path, openmode mode, const char **err);
+			bool is_open(void);
+			void close(void);
+
+			ulong tellg(void);
+			void seekg(ulong pos);
+			void seekg(long off, seekdir dir);
+
+			ulong tellp(void);
+			void seekp(ulong pos);
+			void seekp(long off, seekdir dir);
+
+			int read(u8 *s, ulong n, const char **err);
+			int write(u8 *s, ulong n, const char **err);
+	};
+};
