24d8543389fa6cb63076375375e2fd1b3b1f42b9
Rivest Osz <rivest-oss@proton.me>
2025-04-21 19:28:05 -0300

    Adding UDP class

---
+++ src/net/udp.hpp
@@ -0,0 +1,48 @@
+/* Ox: a general-purpose library.
+** Copyright (C) 2024-2025  Rivest Osz
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#pragma once
+#include "socket.hpp"
+
+namespace Ox {
+	namespace Net {
+		typedef struct udp_packet_t {
+			Socket from;
+			Socket to;
+			ulong length = 0;
+			u8 *data = nullptr;
+
+			bool empty = true;
+		} udp_packet_t;
+
+		class UDP {
+			private:
+				void *implptr = nullptr;
+			
+			public:
+				UDP(void);
+				~UDP(void);
+
+				int init(Error &err);
+				int bind(Socket socket, Error &err);
+				void close(void);
+
+				int read(udp_packet_t *packet, Error &err);
+				int send(udp_packet_t packet, Error &err);
+		};
+	};
+};
